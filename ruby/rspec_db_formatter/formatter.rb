3fba64937f0d27f0c9640c2a0b0b0ae5dc6ca9f2
snackattas <zach.attas@gmail.com>
2024-01-06 22:59:21 -0600

    gh commit sha;

--- ruby/rspec_db_formatter/formatter.rb
+++ ruby/rspec_db_formatter/formatter.rb
@@ -38,7 +38,7 @@ class RSpecDBFormatter < RSpec::Core::Formatters::BaseFormatter
         @build_id = "#{ENV['GITHUB_RUN_ID']}-#{ENV['GITHUB_RUN_ATTEMPT']}"
         @branch = ENV["GITHUB_HEAD_REF"]
         @repo = ENV["GITHUB_REPOSITORY"].split("/")[1..]
-        @git_hash = ENV["GITHUB_SHA"]
+        @git_hash = ENV["GITHUB_COMMIT_SHA"]
         @url = "#{ENV['GITHUB_SERVER_URL']}/#{ENV['GITHUB_REPOSITORY']}/actions/runs/#{ENV['GITHUB_RUN_ID']}"
         @commit_author = ENV["GITHUB_ACTOR"]
         @parallel_process = get_parallel_process_number
